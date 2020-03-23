8f68c2948bc45e848041eed7e43e78d1767c1bb8
adyashakhuntia <adyasha_khuntia@psych.mpg.de>
2020-03-23 17:07:58 +0100

    documentation

--- mean_pixels.py
+++ mean_pixels.py
@@ -2,13 +2,20 @@
 
 import statistics
 
-def calculate_pixel_brightness(x,y,z):
+def calculate_pixel_brightness(x, y, z):
+    """
+    Calculates the brightnss of a pixel from its r,g,b values
+    :param x: the red value of pixel
+    :param y: the green value of pixel
+    :param z: the blue value of pixel
+    :return: the brightness of the pixel
+    """
     return statistics.mean([x, y, z])
 
 pixels = [
     {'red': 100, 'green': 0, 'blue': 0},
     {'red2': 50, 'green2': 100, 'blue2': 0},
 ]
-pixel_brightness = [calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue']) for pixel in pixels
+pixel_brightness = [calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue']) for pixel in pixels]
 mean_brightness = statistics.mean(pixel_brightness)
 print(mean_brightness)
