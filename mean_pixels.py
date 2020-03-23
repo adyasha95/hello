78b8af93cd51fddcabdf04cae4daed7e4bfd74c9
adyashakhuntia <adyasha_khuntia@psych.mpg.de>
2020-03-23 16:58:46 +0100

    remove unnecessary variable

--- mean_pixels.py
+++ mean_pixels.py
@@ -3,15 +3,12 @@
 import statistics
 
 def calculate_pixel_brightness(x,y,z):
-    brightness = statistics.mean([x, y, z])
-    return brightness
+    return statistics.mean([x, y, z])
 
-
-pixel = {'red': 100, 'green': 0, 'blue': 0}
-pixel_brightness = calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue'])
-
-pixel2 = {'red2': 50, 'green2': 100, 'blue2': 0}
-pixel_brightness2 = calculate_pixel_brightness(pixel2['red2'], pixel2['green2'], pixel2['blue2'])
-
-mean_brightness = statistics.mean([pixel_brightness, pixel_brightness2])
+pixels = [
+    {'red': 100, 'green': 0, 'blue': 0},
+    {'red2': 50, 'green2': 100, 'blue2': 0},
+]
+pixel_brightness = [calculate_pixel_brightness(pixel['red'], pixel['green'], pixel['blue']) for pixel in pixels
+mean_brightness = statistics.mean(pixel_brightness)
 print(mean_brightness)
