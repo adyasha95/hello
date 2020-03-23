ccbdee4f16ec33be2fa4f8ea061e7c02ba1dfd34
adyashakhuntia <adyasha_khuntia@psych.mpg.de>
2020-03-23 16:44:23 +0100

    statistics library

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,8 +1,9 @@
 #This is my project
 
+import statistics
 
 def calculate_pixel_brightness(x,y,z):
-    brightness = (x + y + z) / 3
+    brightness = statistics.mean([x, y, z])
     return brightness
 
 
@@ -12,5 +13,5 @@ pixel_brightness = calculate_pixel_brightness(pixel['red'], pixel['green'], pixe
 pixel2 = {'red2': 50, 'green2': 100, 'blue2': 0}
 pixel_brightness2 = calculate_pixel_brightness(pixel2['red2'], pixel2['green2'], pixel2['blue2'])
 
-mean_brightness = (pixel_brightness + pixel_brightness2) / 2
+mean_brightness = statistics.mean([pixel_brightness, pixel_brightness2])
 print(mean_brightness)
