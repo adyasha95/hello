e985f21e8abe584e4156c5e468a82b67f01897c6
adyashakhuntia <adyasha_khuntia@psych.mpg.de>
2020-03-23 16:08:42 +0100

    name changes

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,16 +1,16 @@
-x = 100  # Red value
-x2 = 50
-y = 0  # Green value
-y2 = 100
-z = 0  # Blue value
-z2 = 0
-w = 0  # Readiness value
-w2 = 200
+red = 100  # Red value
+red2 = 50
+green = 0  # Green value
+green2 = 100
+blue = 0  # Blue value
+blue2 = 0
+readiness = 0  # Readiness value
+readiness2 = 200
 
 # Get pixel brightnesses
-result = (x + y + z) / 3
-result2 = (x2 + y2 + z2) / 3
+pixel_brightness = (red + green + blue) / 3
+pixel_brightness2 = (red2 + green2 + blue2) / 3
 
 # Get mean brightness
-result3 = (result + result2) / 2
-print(result3)
+pixel_brightness3 = (pixel_brightness + pixel_brightness2) / 2
+print(pixel_brightness3)
