38f8354749ca4f458b7df2e6802f9bba4f4cf393
adyashakhuntia <adyasha_khuntia@psych.mpg.de>
2020-03-23 16:29:11 +0100

    function

--- mean_pixels.py
+++ mean_pixels.py
@@ -12,8 +12,5 @@ pixel_brightness = calculate_pixel_brightness(red, green, blue)
 red2, green2, blue2 = 50, 100, 0
 pixel_brightness2 = calculate_pixel_brightness(red2, green2, blue2)
 
-readiness = 0
-readiness2 = 200
-
 mean_brightness = (pixel_brightness + pixel_brightness2) / 2
 print(mean_brightness)
