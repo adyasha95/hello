7cd1d223e2b851f6cbb811efacbf3138f4a3cd88
adyashakhuntia <adyasha_khuntia@psych.mpg.de>
2020-03-23 16:11:03 +0100

    comment change

--- mean_pixels.py
+++ mean_pixels.py
@@ -1,16 +1,16 @@
-red = 100  # Red value
+#This is my project
+
+red = 100
 red2 = 50
-green = 0  # Green value
+green = 0
 green2 = 100
-blue = 0  # Blue value
+blue = 0
 blue2 = 0
-readiness = 0  # Readiness value
+readiness = 0
 readiness2 = 200
 
-# Get pixel brightnesses
 pixel_brightness = (red + green + blue) / 3
 pixel_brightness2 = (red2 + green2 + blue2) / 3
 
-# Get mean brightness
 mean_brightness = (pixel_brightness + pixel_brightness2) / 2
 print(mean_brightness)
